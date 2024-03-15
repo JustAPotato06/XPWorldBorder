aee0651c989f37872d5011e8fe3c7c909ea05137
Potato <potatodevelopment06@gmail.com>
2024-03-15 12:37:29 -0600

    Countdown sound change

--- src/main/java/dev/potato/xpworldborder/tasks/KillCountdownSoundTask.java
+++ src/main/java/dev/potato/xpworldborder/tasks/KillCountdownSoundTask.java
@@ -5,7 +5,6 @@ import net.kyori.adventure.text.format.NamedTextColor;
 import org.bukkit.Bukkit;
 import org.bukkit.Instrument;
 import org.bukkit.Note;
-import org.bukkit.Sound;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
 import org.bukkit.scheduler.BukkitRunnable;
@@ -34,53 +33,46 @@ public class KillCountdownSoundTask extends BukkitRunnable {
         }
 
         if (counter == 1) {
-            player.playSound(player.getLocation(), Sound.UI_BUTTON_CLICK, 1, 1);
-            playClickForAllPlayers(shouldOthersHear);
+            Instrument sticks = Instrument.STICKS;
+            Note note = Note.natural(0, Note.Tone.C);
+            player.playNote(player.getLocation(), sticks, note);
+            playNoteForAllPlayers(shouldOthersHear, sticks, note);
         }
 
         boolean onGreen = currentColor == NamedTextColor.GREEN && counter == 1;
         boolean onYellow = currentColor == NamedTextColor.YELLOW && (counter == 1 || counter == 11);
         boolean onOrange = currentColor == NamedTextColor.GOLD && (counter == 1 || counter == 6 || counter == 11 || counter == 16);
         boolean onRed = currentColor == NamedTextColor.RED && !(counter == 1 || counter == 4 || counter == 7 || counter == 10 || counter == 13 || counter == 16 || counter == 19);
+        Instrument pling = Instrument.PLING;
 
         if (onGreen) {
             Note note = Note.natural(0, Note.Tone.A);
-            player.playNote(player.getLocation(), Instrument.PLING, note);
-            playNoteForAllPlayers(shouldOthersHear, note);
+            player.playNote(player.getLocation(), pling, note);
+            playNoteForAllPlayers(shouldOthersHear, pling, note);
         } else if (onYellow) {
             Note note = Note.sharp(0, Note.Tone.A);
-            player.playNote(player.getLocation(), Instrument.PLING, note);
-            playNoteForAllPlayers(shouldOthersHear, note);
+            player.playNote(player.getLocation(), pling, note);
+            playNoteForAllPlayers(shouldOthersHear, pling, note);
         } else if (onOrange) {
             Note note = Note.natural(0, Note.Tone.B);
-            player.playNote(player.getLocation(), Instrument.PLING, note);
-            playNoteForAllPlayers(shouldOthersHear, note);
+            player.playNote(player.getLocation(), pling, note);
+            playNoteForAllPlayers(shouldOthersHear, pling, note);
         } else if (onRed) {
             Note note = Note.natural(0, Note.Tone.C);
-            player.playNote(player.getLocation(), Instrument.PLING, note);
-            playNoteForAllPlayers(shouldOthersHear, note);
+            player.playNote(player.getLocation(), pling, note);
+            playNoteForAllPlayers(shouldOthersHear, pling, note);
         }
 
         if (counter == 20) counter = 1;
         else counter++;
     }
 
-    private void playClickForAllPlayers(boolean shouldOthersHear) {
+    private void playNoteForAllPlayers(boolean shouldOthersHear, Instrument instrument, Note note) {
         if (shouldOthersHear) {
             for (Entity currentEntity : player.getNearbyEntities(50, 50, 50)) {
                 if (!(currentEntity instanceof Player currentPlayer)) continue;
                 if (worldBorderManager.getCountdownTasks().containsKey(currentPlayer)) continue;
-                currentPlayer.playSound(player.getLocation(), Sound.UI_BUTTON_CLICK, 1, 1);
-            }
-        }
-    }
-
-    private void playNoteForAllPlayers(boolean shouldOthersHear, Note note) {
-        if (shouldOthersHear) {
-            for (Entity currentEntity : player.getNearbyEntities(50, 50, 50)) {
-                if (!(currentEntity instanceof Player currentPlayer)) continue;
-                if (worldBorderManager.getCountdownTasks().containsKey(currentPlayer)) continue;
-                currentPlayer.playNote(player.getLocation(), Instrument.PLING, note);
+                currentPlayer.playNote(player.getLocation(), instrument, note);
             }
         }
     }
